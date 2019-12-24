86ed9c79dd7c76b4c7872b1a0c0a2db8b4cc7d4c
Patrick Gustafson <patrickgus@charter.net>
2019-12-23 16:12:03 -0800

    import GoogleApp to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import GoogleApp from "../GoogleApp/GoogleApp";
 import "./App.css";
 
 export default class App extends Component {
   render() {
     return (
       <main className="App">
-        { /* content */ }
+        <GoogleApp />
       </main>
     );
   }
